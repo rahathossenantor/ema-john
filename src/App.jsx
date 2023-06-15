27b6580f1429b745cdb065bf37bdba0172d49339
mdrahathossenantor <md.rahathossenantor@gmail.com>
2023-06-16 01:42:28 +0600

    project logo added

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,13 @@
-import {useState} from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+// import {useState} from "react"
+import "./App.css"
+import Header from "./components/Header/Header"
 
 function App() {
 
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <Header></Header>
       </div>
     </>
   )
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,13 @@
+// import React from "react"
+import logo from "../../images/logo.png"
+import "./Header.css"
+
+const Header = () => {
+    return (
+        <div className="header">
+            <img src={logo} alt="ema-john" />
+        </div>
+    );
+};
+
+export default Header
